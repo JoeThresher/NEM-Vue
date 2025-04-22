29a5c17deb808e20e09ef7488a916f001a6dc9b0
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-04-22 16:00:28 -0400

    Split HeaderBar into its own component and added light/dark toggle

--- src/App.vue
+++ src/App.vue
@@ -1,70 +1,10 @@
 <script setup>
 // import { RouterLink, RouterView } from 'vue-router'
+import HeaderBar from './components/HeaderBar.vue'
 </script>
 
 <template>
-  <header class="container">
-    <nav>
-      <ul>
-        <li>
-          <a href="/NEM-Mockup" class="contrast"
-            ><img src="/src/assets/NEM.jpg" class="header-logo" alt="Logo"
-          /></a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-          <details class="dropdown">
-            <summary>Tournaments</summary>
-            <ul dir="rtl">
-              <li>
-                <a
-                  href="https://calendar.google.com/calendar/u/0/embed?src=86oup09opi66vbhshrftu4uijs@group.calendar.google.com&ctz=America/New_York"
-                  target="_blank"
-                  >Calendar</a
-                >
-              </li>
-              <li>
-                <a href="src/pages/locals.html">Locals</a>
-              </li>
-              <li>
-                <a href="src/pages/regionals.html">Regionals</a>
-              </li>
-            </ul>
-          </details>
-        </li>
-        <li>
-          <button
-            class="theme-toggle"
-            id="theme-toggle"
-            type="button"
-            title="Toggle theme"
-            aria-label="Toggle theme"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              width="1em"
-              height="1em"
-              fill="currentColor"
-              class="theme-toggle__expand"
-              viewBox="0 0 32 32"
-            >
-              <clipPath id="theme-toggle__expand__cutout">
-                <path d="M0-11h25a1 1 0 0017 13v30H0Z" />
-              </clipPath>
-              <g clip-path="url(#theme-toggle__expand__cutout)">
-                <circle cx="16" cy="16" r="8.4" />
-                <path
-                  d="M18.3 3.2c0 1.3-1 2.3-2.3 2.3s-2.3-1-2.3-2.3S14.7.9 16 .9s2.3 1 2.3 2.3zm-4.6 25.6c0-1.3 1-2.3 2.3-2.3s2.3 1 2.3 2.3-1 2.3-2.3 2.3-2.3-1-2.3-2.3zm15.1-10.5c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zM3.2 13.7c1.3 0 2.3 1 2.3 2.3s-1 2.3-2.3 2.3S.9 17.3.9 16s1-2.3 2.3-2.3zm5.8-7C9 7.9 7.9 9 6.7 9S4.4 8 4.4 6.7s1-2.3 2.3-2.3S9 5.4 9 6.7zm16.3 21c-1.3 0-2.3-1-2.3-2.3s1-2.3 2.3-2.3 2.3 1 2.3 2.3-1 2.3-2.3 2.3zm2.4-21c0 1.3-1 2.3-2.3 2.3S23 7.9 23 6.7s1-2.3 2.3-2.3 2.4 1 2.4 2.3zM6.7 23C8 23 9 24 9 25.3s-1 2.3-2.3 2.3-2.3-1-2.3-2.3 1-2.3 2.3-2.3z"
-                />
-              </g>
-            </svg>
-          </button>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <HeaderBar />
 
   <section id="hero">
     <div class="container hero-content">
@@ -115,29 +55,6 @@
 </template>
 
 <style scoped>
-header {
-  z-index: 2;
-  position: sticky;
-  top: 0;
-  background-color: var(--pico-background-color);
-}
-
-.header-logo {
-  width: 60px;
-  height: auto;
-  border-radius: 50%;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.header-logo:hover {
-  filter: drop-shadow(0 0 0.5em var(--pico-color-yellow-100));
-}
-
-#theme-toggle {
-  color: var(--pico-contrast);
-}
-
 .hero-content img {
   /* max-width: 25%; */
   width: clamp(20%, 30%, 300px);
